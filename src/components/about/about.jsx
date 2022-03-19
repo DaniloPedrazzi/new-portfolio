887da8c8674c9407266f121044bb168ebf8c5992
Danilo <danilo.justino1@gmail.com>
2022-03-18 22:29:40 -0300

    Nav

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,7 +3,19 @@ import './about.css'
 
 function about() {
   return (
-    <div>about</div>
+    <section id='about'>
+      <h5>Saiba Mais</h5>
+      <h2>Sobre Mim</h2>
+
+      <div className="container about_container">
+        <div className="about_me">
+
+        </div>
+        <div className="about_content">
+          
+        </div>
+      </div>
+    </section>
   )
 }
 
